6c38bd1117b024988fdf326e10af48be1a2a566e
Ravi <lcs2020043@iiitl.ac.in>
2024-01-18 14:11:18 +0530

    added tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.jpg";
+
+export default function Header() {
+  return (
+    <header className=" flex justify-between mx-16 items-center my-8">
+      <div className=" flex items-center">
+        <img src={logo} alt="A restaurant." className=" h-16 w-16 rounded-full border-yellow-300 border-solid border-2" />
+        <h1 className=" mx-4 font-bold text-2xl">Food Shop</h1>
+      </div>
+      <nav>
+        <button className=" bg-white text-black px-2">Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
