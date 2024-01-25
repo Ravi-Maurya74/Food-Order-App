2177c559f7c4d035ced04834f20d02b5a6d757e7
Ravi <lcs2020043@iiitl.ac.in>
2024-01-25 10:17:17 +0530

    Error

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,23 @@
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {};
 
 export default function Meals() {
-  const { data:loadedMeals, isLoading, error } = useHttp("http://localhost:3000/meals",{},[]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+  if (isLoading) {
+    return <p className="center">Loading...</p>;
+  }
+
+  if (error) {
+    return <Error title="Failed to fetch meals!" message={error} />;
+  }
 
   return (
     <ul className=" grid grid-cols-2 md:grid-cols-3">
@@ -12,4 +27,3 @@ export default function Meals() {
     </ul>
   );
 }
- 
